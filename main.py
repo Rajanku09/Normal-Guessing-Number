4161eb032d0eee9d770b14a984cd3636bf5dd7b9
Rajan Kumar <70188005+Rajanku09@users.noreply.github.com>
2021-03-17 20:35:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+winning_number=random.randint(1,100)
+guess_number=int(input("enter your number"))
+
+count=1
+flag=False
+
+while not flag:
+    if guess_number==winning_number:
+        print("You Win in {} Times ".format(count))
+        flag=True
+
+    elif guess_number<winning_number:
+        print("enter above")
+
+    elif guess_number>winning_number:
+        print("enter below")
+
+
+    count += 1
+    guess_number = int(input("enter your number"))
+
+
+
